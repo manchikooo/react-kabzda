79f8ac35b6ab7d1364c7bc12c82493187b8b399a
manchikooo <manchiko.man31@mail.ru>
2022-01-15 15:31:25 +0300

    пояснение как в аргумент функции попадает наше значение из стейта

--- src/components/UseState/UseState.tsx
+++ src/components/UseState/UseState.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useState} from 'react';
 
 let generateData = () => {
     console.log('generateData')
@@ -12,8 +12,14 @@ const UseState = () => {
 
     const [counter, setCounter] = useState(generateData) // реакт вызовет функцию единожды, запомнит значение, которое она вернет и засетает его в значение стейта. Дальше будет меняться значение в стейте и функция повторно вызываться не будет. Работает схоже с useMemo
 
+    const changer = (state: number) => { // пояснение как работает в 22 строке
+        return state + 1
+    }
+
     return (
         <>
+            <button onClick={() => setCounter(changer)}>+</button>
+            {/* из-за setAction реакт понимает, что в функции вместо state в аргументе функции сидит наше значение из useState */}
             <button onClick={() => setCounter(state => state + 1)}>+</button>
             {counter}
             <button onClick={() => setCounter(0)}>reset</button>
